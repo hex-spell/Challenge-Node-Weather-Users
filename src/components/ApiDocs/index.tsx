aa5525062d7a2414117e3bb529dca54afd3222ac
Vagus-art <agustin.favo@gmail.com>
2020-10-04 13:33:17 -0300

    agregado otro boton de clipboard y un poco de texto

--- src/components/ApiDocs/index.tsx
+++ src/components/ApiDocs/index.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { Text, Heading, Divider, Box, Code } from "@chakra-ui/core";
+import {
+  Text,
+  Heading,
+  Divider,
+  Box,
+  Code,
+  useToast,
+  IconButton,
+} from "@chakra-ui/core";
 import { Link } from "react-router-dom";
+import { useClipboard } from "use-clipboard-copy";
 import { Container } from "../Layout";
 
 const ApiDocs: React.FC = () => {
+  const clipboard = useClipboard();
+  const toast = useToast();
+  const copyToClipboard = (data: string) => {
+    clipboard.copy(data);
+    toast({
+      title: "Endpoint copiado al portapapeles!",
+      description: "No te olvides de cambiar todo lo rojo por tus datos.",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
+  };
   return (
     <Container p="20px" bg="white" rounded="md" mt="5px">
       <Heading as="h1" size="xl">
@@ -22,8 +43,8 @@ const ApiDocs: React.FC = () => {
       </Heading>
       <Divider opacity={0} />
       <Text>
-        Puede llamar por nombre de ciudad. Vas a necesitar una api key para
-        hacer esta petición, para obtener una debes ir al panel de{" "}
+        La consulta se hace por nombre de ciudad. Vas a necesitar una api key
+        para hacer esta petición, para obtener una debes ir al panel de{" "}
         <Link to="/keys" style={{ color: "teal", fontWeight: "bold" }}>
           Mis Llaves
         </Link>
@@ -31,9 +52,20 @@ const ApiDocs: React.FC = () => {
       <Divider opacity={0} />
       <Box display="flex" flexWrap="wrap">
         <Code>{"https://node-weather-challenge.herokuapp.com/weather?q="}</Code>
-        <Code variantColor="orange">{"{Nombre de ciudad}"}</Code>
+        <Code variantColor="red">{"Ciudad"}</Code>
         <Code>{"&appid="}</Code>
-        <Code variantColor="orange">{"{API key}"}</Code>
+        <Code variantColor="red">{"API key"}</Code>
+        <IconButton
+          onClick={() =>
+            copyToClipboard(
+              "https://node-weather-challenge.herokuapp.com/weather?q=Ciudad&appid=API key"
+            )
+          }
+          aria-label="delete api key"
+          icon="copy"
+          variant="ghost"
+          variantColor="teal"
+        />
       </Box>
     </Container>
   );
